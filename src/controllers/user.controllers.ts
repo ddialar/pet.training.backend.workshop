ba3ea29a25e728cda624fb276f38bfc6de551c5d
ddialar <ddialar@gmail.com>
2022-06-01 18:47:15 +0100

    refactor(user): move error handling from controller to repository

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -2,15 +2,6 @@ import { logger } from '@logger'
 import { hashPassword } from '@services'
 import { userRepositories } from '@repositories'
 import { SigninRequest, UserData, UserProfileData } from '@types'
-import { CreateUserError, CreateUserProfileError, UserAlreadyExistsError } from '@errors'
-
-const findUserAndErrorIfExists = async (query: Partial<UserData>): Promise<void> => {
-  const user = await userRepositories.getUser(query)
-  if (user) {
-    logger.error({ query }, 'User already exists')
-    throw new UserAlreadyExistsError()
-  }
-}
 
 const persistNewUser = async ({ email, password }: Pick<SigninRequest, 'email' | 'password'>): Promise<UserData> => {
   // TODO Hash the password
@@ -21,33 +12,25 @@ const persistNewUser = async ({ email, password }: Pick<SigninRequest, 'email' |
     password: hashedPassword
   }
 
-  try {
-    return await userRepositories.createUser(parsedUser)
-  } catch (error) {
-    throw new CreateUserError((<Error>error).message)
-  }
+  return await userRepositories.createUser(parsedUser)
 }
 
 const persistNewUserProfile = async ({ id }: UserData, { email, firstName, lastName }: Pick<SigninRequest, 'email' | 'firstName' | 'lastName'>): Promise<UserProfileData> => {
-  try {
-    const parsedUserProfile = {
-      email,
-      firstName,
-      lastName,
-      userId: id
-    }
-    // TODO Persist the user's profile
-    return await userRepositories.createProfile(parsedUserProfile)
-  } catch (error) {
-    throw new CreateUserProfileError((<Error>error).message)
+  const parsedUserProfile = {
+    email,
+    firstName,
+    lastName,
+    userId: id
   }
+  // TODO Persist the user's profile
+  return await userRepositories.createProfile(parsedUserProfile)
 }
 
 export const signin = async ({ email, password, firstName, lastName }: SigninRequest) => {
   logger.info({ method: 'signin controller', email, firstName, lastName }, 'New user signin process started')
 
   // TODO Check whether the user already exists
-  await findUserAndErrorIfExists({ username: email })
+  await userRepositories.findUserAndErrorIfExists({ username: email })
   // TODO Persist new user
   const persistedUser = await persistNewUser({ email, password })
   // TODO Create the profile
--- src/infrastructure/repositories/user.repositories.ts
+++ src/infrastructure/repositories/user.repositories.ts
@@ -1,11 +1,43 @@
+import { logger } from '@logger'
 import { userRequests } from '@orm'
+import { CreateUserError, CreateUserProfileError, UserAlreadyExistsError, UserNotFoundError } from '@errors'
 import { NewUserData, UserData, UserProfileData } from '@types'
 
-export const createUser = async (newUser: NewUserData): Promise<UserData> =>
-  userRequests.createUser(newUser)
+export const createUser = async (newUser: NewUserData): Promise<UserData> => {
+  try {
+    return await userRequests.createUser(newUser)
+  } catch (error) {
+    logger.error({ method: 'repository createUser', newUser }, 'User creation error')
+    throw new CreateUserError((<Error>error).message)
+  }
+}
 
-export const createProfile = async (newUserProfile: UserProfileData): Promise<UserProfileData> =>
-  userRequests.createUserProfile(newUserProfile)
+export const createProfile = async (newUserProfile: UserProfileData): Promise<UserProfileData> => {
+  try {
+    return await userRequests.createUserProfile(newUserProfile)
+  } catch (error) {
+    logger.error({ method: 'repository createProfile', newUserProfile }, 'User profile creation error')
+    throw new CreateUserProfileError((<Error>error).message)
+  }
+}
 
 export const getUser = async (searchParms: Partial<UserData>): Promise<UserData | undefined> =>
   userRequests.getUser(searchParms)
+
+export const findUserAndErrorIfExists = async (query: Partial<UserData>): Promise<void> => {
+  const user = await getUser(query)
+  if (user) {
+    logger.error({ method: 'repository findUserAndErrorIfExists', query }, 'User already exists')
+    throw new UserAlreadyExistsError()
+  }
+}
+
+export const findUserAndErrorIfNotExists = async (query: Partial<UserData>): Promise<UserData> => {
+  const user = await getUser(query)
+  if (!user) {
+    logger.error({ method: 'repository findUserAndErrorIfNotExists', query }, 'User not found')
+    throw new UserNotFoundError()
+  }
+
+  return user
+}
